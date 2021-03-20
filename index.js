01fc06a93010e1d03476503ed955d193531a90d5
thaumatrge <benjychada@icloud.com>
2021-03-20 18:08:34 +0000

    Favicon now working

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const app = require('express')();
+const express = require('express');
+const app = express()
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
+const favicon = require('serve-favicon')
+const path = require('path')
+
 const port = process.env.PORT || 3001;
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', (socket) => {
