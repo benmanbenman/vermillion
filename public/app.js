b827c33f7e590d904442a7111f0242717c267795
thaumatrge <benjychada@icloud.com>
2021-03-21 14:18:29 +0000

    Only logged in users can speak.

--- public/app.js
+++ public/app.js
@@ -24,4 +24,22 @@ function onSignIn(googleUser) {
     document.getElementById("pfp").src = profile.getImageUrl()
     document.getElementById("signin").style = "display: none"
     logged_in = true;
+
+    // Let people who signed in talk
+
+    var message_input = document.createElement('input');
+    var button = document.createElement('button')
+
+    // var m = "<input id=\"input\" placeholder=\"Message everyone in this room\" autocomplete=\"off\" autofocus></input>"
+    // var b = "<button></button>"
+
+    message_input.id = "input";
+    message_input.placeholder = "Message everyone in this room";
+    message_input.autocomplete = "off";
+    message_input.autofocus = true;
+
+    f = document.getElementById("form");
+
+    f.appendChild(message_input);
+    f.appendChild(button);
 }
