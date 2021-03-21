863813657ade35e992ec1ca882a90401f0672f46
thaumatrge <benjychada@icloud.com>
2021-03-21 09:33:57 +0000

    RWD

--- public/app.js
+++ public/app.js
@@ -1,24 +1,3 @@
-function addDarkmodeWidget() {
-    const options = {
-        bottom: '94vh', // default: '32px'
-        right: 'unset', // default: '32px'
-        left: '48.5vw', // default: 'unset'
-        time: '.75s', // default: '0.3s'
-        mixColor: '#fff', // default: '#fff'
-        backgroundColor: '#fff',  // default: '#fff'
-        buttonColorDark: '#000',  // default: '#100f2c'
-        buttonColorLight: '#fff', // default: '#fff'
-        saveInCookies: true, // default: true,
-        label: '', // default: ''
-        autoMatchOsTheme: true // default: true
-    }
-
-    const darkmode = new Darkmode(options);
-    darkmode.showWidget();
-}
-
-window.addEventListener('load', addDarkmodeWidget);
-
 function onSignIn(googleUser) {
     let profile = googleUser.getBasicProfile();
     sessionStorage.setItem("name", profile.getName())
@@ -28,15 +7,17 @@ function onSignIn(googleUser) {
 
     var pfp = document.createElement("img");
     pfp.id = "pfp";
+    pfp_text.id = "pfp_text";
+    pfp.classList.add("pfp")
+    pfp_text.classList.add("pfp_text")
 
-    pfp_text.style = "position: fixed; top: 1.5vh; left: 6vw;"
     pfp_text.appendChild(text);
-    pfp.style = "position: fixed; top: 1vh; left: 1vw; width: 60.5px; height: auto"
 
-    var element = document.getElementById("top");
+    var element = document.getElementById("header");
 
     element.appendChild(pfp_text);
     element.appendChild(pfp);
 
     document.getElementById("pfp").src = profile.getImageUrl()
+    document.getElementById("signin").style = "display: none"
 }
