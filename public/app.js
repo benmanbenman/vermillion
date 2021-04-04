04f4cbc1c0baad606fd5e515affa6b8249dbf2f8
thaumatrge <benjychada@icloud.com>
2021-04-04 15:11:44 +0100

    Saves dark mode in localstorage

--- public/app.js
+++ public/app.js
@@ -1,6 +1,25 @@
+console.log(typeof(localStorage.getItem("darkmode")) + ": " + localStorage.getItem("darkmode"))
+
+switch(localStorage.getItem("darkmode")) {
+	case null:
+		console.log("none")
+		break;
+	case "true":
+		console.log("true")
+		document.getElementById("body").classList.add("dark");
+		break;
+	case "false":
+		console.log("false")
+		document.getElementById("body").classList.remove("dark");
+		break;
+	default:
+		console.log("other")
+}
+
 // Dark mode
 const dark = () => {
-    document.getElementById("body").classList.toggle("dark");
+	document.getElementById("body").classList.toggle("dark");
+	localStorage.setItem("darkmode", document.getElementById("body").classList.contains("dark"));
 };
 
 function onSignIn(googleUser) {
