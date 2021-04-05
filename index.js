2535f55c4bdcceeee315fbd71eaa17262d4f4732
thaumatrge <benjychada@icloud.com>
2021-04-05 09:38:35 +0100

    Tweaks

--- index.js
+++ index.js
@@ -1,57 +1,58 @@
-const express = require('express');
-const app = express()
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const fs = require('fs')
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const fs = require("fs");
 
 const filename = "messages.txt";
 
-const favicon = require('serve-favicon')
-const path = require('path')
+const favicon = require("serve-favicon");
+const path = require("path");
 
 const port = process.env.PORT || 3001;
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
-
-app.use(express.static(path.join(__dirname, 'public')))
-app.get('/', (req, res) => {
-  res.redirect('index.html')
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+	res.redirect("index.html");
 });
 
-io.on('connection', (socket) => {
-  console.log('connection!');
-  socket.emit('connection', 'person join');
+io.on("connection", (socket) => {
+	console.log("connection!");
+	socket.emit("connection", "person join");
 
-  messages = fs.readFileSync('messages.txt', 'utf-8').split(/\r?\n/)
-  messages.forEach((l) => {
-    console.log(socket.id)
-    io.to(socket.id).emit('chat message', l);
-  });
+	messages = fs.readFileSync("messages.txt", "utf-8").split(/\r?\n/);
+	messages.forEach((l) => {
+		console.log(socket.id);
+		io.to(socket.id).emit("chat message", l);
+	});
 
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-    console.log(msg)
-    data = '\n' + msg
-    fs.open(filename, "a", (err, fd)=>{
-      if(err){
-          console.log(err.message);
-      }else{
-          fs.write(fd, data, (err, bytes)=>{
-              if(err){
-                  console.log(err.message);
-              }else{
-                  console.log(bytes +' bytes written');
-              }
-          })
-      }
-  })
-  });
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
+	socket.on("chat message", (msg) => {
+		io.emit("chat message", msg);
+		console.log(msg);
+		data = "\n" + msg;
+		fs.open(filename, "a", (err, fd) => {
+			if (err) {
+				console.log(err.message);
+			} else {
+				fs.write(fd, data, (err, bytes) => {
+					if (err) {
+						console.log(err.message);
+					} else {
+						console.log(
+							bytes + " bytes written"
+						);
+					}
+				});
+			}
+		});
+	});
+	socket.on("disconnect", () => {
+		console.log("user disconnected");
+	});
 });
 
 http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+	console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
--- public/app.js
+++ public/app.js
@@ -1,72 +1,71 @@
-switch(localStorage.getItem("darkmode")) {
+switch (localStorage.getItem("darkmode")) {
 	case null:
-		console.log("none")
+		console.log("none");
 		break;
 	case "true":
-		console.log("true")
+		console.log("true");
 		document.getElementById("body").classList.add("dark");
 		break;
 	case "false":
-		console.log("false")
+		console.log("false");
 		document.getElementById("body").classList.remove("dark");
 		break;
 	default:
-		console.log("other")
+		console.log("other");
 }
-
-if (sessionStorage.getItem("refreshed") == null) {
-	window.location.reload();
-}
-
-sessionStorage.setItem("refreshed", true)
 
 // Dark mode
 const dark = () => {
 	document.getElementById("body").classList.toggle("dark");
-	localStorage.setItem("darkmode", document.getElementById("body").classList.contains("dark"));
+	localStorage.setItem(
+		"darkmode",
+		document.getElementById("body").classList.contains("dark")
+	);
 };
 
 function onSignIn(googleUser) {
-    let profile = googleUser.getBasicProfile();
-    sessionStorage.setItem("name", profile.getName())
-    sessionStorage.setItem("profile", profile.getImageUrl())
+	let profile = googleUser.getBasicProfile();
+	sessionStorage.setItem("name", profile.getName());
+	sessionStorage.setItem("profile", profile.getImageUrl());
 
-    // Add profile to the header
-    var pfp_text = document.createElement("p");
-    var text = document.createTextNode(profile.getName());
+	// Add profile to the header
+	var pfp_text = document.createElement("p");
+	var text = document.createTextNode(profile.getName());
 
-    var pfp = document.createElement("img");
+	var pfp = document.createElement("img");
 
-    pfp.id = "pfp";
-    pfp_text.id = "pfp_text";
+	pfp.id = "pfp";
+	pfp_text.id = "pfp_text";
 
-    // Dark mode
-    pfp.onclick = dark
+	// Dark mode
+	pfp.onclick = dark;
 
-    pfp.classList.add("pfp")
-    pfp_text.classList.add("pfp_text")
+	pfp.classList.add("pfp");
+	pfp_text.classList.add("pfp_text");
 
-    pfp_text.appendChild(text);
+	pfp_text.appendChild(text);
 
-    var element = document.getElementById("header");
+	var element = document.getElementById("header");
 
-    element.appendChild(pfp_text);
-    element.appendChild(pfp);
+	element.appendChild(pfp_text);
+	element.appendChild(pfp);
 
-    document.getElementById("pfp").src = profile.getImageUrl();
-    document.getElementById("signin").style = "display: none";
+	document.getElementById("pfp").src = profile.getImageUrl();
+	document.getElementById("signin").style = "display: none";
 
-    // Let people who signed in talk
-    var message_input = document.createElement('input');
-    var button = document.createElement('button')
+	// Let people who signed in talk
+	var message_input = document.createElement("input");
+	var button = document.createElement("button");
 
-    message_input.id = "input";
-    message_input.placeholder = "Message everyone in this room";
-    message_input.autocomplete = "off";
-    message_input.onkeypress = function(){message_input.value = emojify(message_input.value)}
-    f = document.getElementById("form");
+	message_input.id = "input";
+	message_input.placeholder = "Message everyone in this room";
+	message_input.autocomplete = "off";
+	message_input.onkeypress = function () {
+		message_input.value = emojify(message_input.value);
+	};
+	f = document.getElementById("form");
 
-    f.appendChild(message_input);
+	f.appendChild(message_input);
 
-    f.appendChild(button);
+	f.appendChild(button);
 }
